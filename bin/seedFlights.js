26fbf65f9db3557f60a2d9e95f2d9a503e3695e1
juliajulia89 <julkajanicka@gmail.com>
2022-03-26 18:29:08 +0100

    seed flights

---
+++ bin/seedFlights.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+require("dotenv/config");
+const MONGODB = process.env.MONGODB_URI;
+const Phone = require("../models/flight");
+
+const seedFlights = async () => {
+  mongoose
+    .connect(MONGODB, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    .then((db) => console.log("Connected to the database succesfully"))
+    .catch((err) => console.log("Connection failed"));
+
+  try {
+
+    
+  } catch (error) {
+    console.log(error);
+  }
+  mongoose.disconnect();
+};
+
+seedFlights();

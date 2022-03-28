1966394fe9d7148e68a6291185499abf5ec156b0
juliajulia89 <julkajanicka@gmail.com>
2022-03-28 04:27:05 +0200

    typo

--- models/flight.js
+++ models/flight.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const { Schema, model } = mongoose;
 
 const flightSchema = new Schema({
-  commentid: { type: Number, required: true },
+  commentId: { type: Number, required: true },
   comment: { type: String, required: true },
   date: { type: Date, required: true, default: Date.now },
   userId: { type: Number, required: true },
